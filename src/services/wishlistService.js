ac287cb4fd91cef9c82ad520d0b6cdbbf734682f
Eman Radwan <emanalaa11499@gmail.com>
2025-06-08 00:11:28 +0300

    create product card

--- src/services/wishlistService.js
+++ src/services/wishlistService.js
@@ -1,1 +1,9 @@
+import api from "../utils/apiUrl";
+
 //just a js file for storing api methods related to wishlist
+export const getAllUserWishlist = async () => {
+  const res = await api.get("/wishlist?all=true");
+  console.log(res);
+
+  return res.data;
+};
