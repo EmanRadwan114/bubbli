eced5ad4186fdd877ae043b88e857adf9e40a866
Doooaa <d98744330@gmail.com>
2025-06-09 00:17:53 +0300

    spinner add display

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,42 +1,109 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const LoadingSpinner = () => {
   return (
     <StyledWrapper>
       <div className="spinner-container">
-        <svg height="108px" width="108px" viewBox="0 0 128 128" className="loader">
+        <svg
+          height="108px"
+          width="108px"
+          viewBox="0 0 128 128"
+          className="loader"
+        >
           <defs>
             <clipPath id="loader-eyes">
-              <circle transform="rotate(-40,64,64) translate(0,-56)" r={8} cy={64} cx={64} className="loader__eye1" />
-              <circle transform="rotate(40,64,64) translate(0,-56)" r={8} cy={64} cx={64} className="loader__eye2" />
+              <circle
+                transform="rotate(-40,64,64) translate(0,-56)"
+                r={8}
+                cy={64}
+                cx={64}
+                className="loader__eye1"
+              />
+              <circle
+                transform="rotate(40,64,64) translate(0,-56)"
+                r={8}
+                cy={64}
+                cx={64}
+                className="loader__eye2"
+              />
             </clipPath>
             <linearGradient y2={1} x2={0} y1={0} x1={0} id="loader-grad">
               <stop stopColor="#000" offset="0%" />
               <stop stopColor="#fff" offset="100%" />
             </linearGradient>
-            <linearGradient id="custom-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
-               <stop offset="0%" stopColor="#c74029" />
+            <linearGradient
+              id="custom-gradient"
+              x1="0%"
+              y1="0%"
+              x2="100%"
+              y2="100%"
+            >
+              <stop offset="0%" stopColor="#c74029" />
               <stop offset="50%" stopColor="#c71000" />
               <stop offset="100%" stopColor="#c70029" />
             </linearGradient>
             <mask id="loader-mask">
-              <rect fill="url(#loader-grad)" height={128} width={128} y={0} x={0} />
+              <rect
+                fill="url(#loader-grad)"
+                height={128}
+                width={128}
+                y={0}
+                x={0}
+              />
             </mask>
           </defs>
-          <g strokeDasharray="175.93 351.86" strokeWidth={12} strokeLinecap="round">
+          <g
+            strokeDasharray="175.93 351.86"
+            strokeWidth={12}
+            strokeLinecap="round"
+          >
             <g>
-              <rect clipPath="url(#loader-eyes)" height={64} width={128} fill="url(#custom-gradient)" />
+              <rect
+                clipPath="url(#loader-eyes)"
+                height={64}
+                width={128}
+                fill="url(#custom-gradient)"
+              />
               <g stroke="url(#custom-gradient)" fill="none">
-                <circle transform="rotate(180,64,64)" r={56} cy={64} cx={64} className="loader__mouth1" />
-                <circle transform="rotate(0,64,64)" r={56} cy={64} cx={64} className="loader__mouth2" />
+                <circle
+                  transform="rotate(180,64,64)"
+                  r={56}
+                  cy={64}
+                  cx={64}
+                  className="loader__mouth1"
+                />
+                <circle
+                  transform="rotate(0,64,64)"
+                  r={56}
+                  cy={64}
+                  cx={64}
+                  className="loader__mouth2"
+                />
               </g>
             </g>
             <g mask="url(#loader-mask)">
-              <rect clipPath="url(#loader-eyes)" height={64} width={128} fill="url(#custom-gradient)" />
+              <rect
+                clipPath="url(#loader-eyes)"
+                height={64}
+                width={128}
+                fill="url(#custom-gradient)"
+              />
               <g stroke="url(#custom-gradient)" fill="none">
-                <circle transform="rotate(180,64,64)" r={56} cy={64} cx={64} className="loader__mouth1" />
-                <circle transform="rotate(0,64,64)" r={56} cy={64} cx={64} className="loader__mouth2" />
+                <circle
+                  transform="rotate(180,64,64)"
+                  r={56}
+                  cy={64}
+                  cx={64}
+                  className="loader__mouth1"
+                />
+                <circle
+                  transform="rotate(0,64,64)"
+                  r={56}
+                  cy={64}
+                  cx={64}
+                  className="loader__mouth2"
+                />
               </g>
             </g>
           </g>
@@ -50,9 +117,14 @@ const StyledWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
+  height: 100vh;
   width: 100%;
-  
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
   .spinner-container {
     display: flex;
     justify-content: center;
