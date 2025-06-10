f656f4fc8a1bfca190465441bf39ecfda5e84fe4
Nouran-Alaa <nouranalaa81@gmail.com>
2025-06-11 00:24:39 +0300

    Coupon field main site

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import CategoryCard from "../../components/CategoryCard/CategoryCard";
+import Coupons from "../../components/Coupons/Coupons";
 import HomeHero from "../../components/HomeHero/HomeHero";
 
 export default function Home() {
   return (
     <div style={{ minHeight: "500vh" }}>
       <HomeHero></HomeHero>
+      <Coupons />
       <CategoryCard />
     </div>
   );
