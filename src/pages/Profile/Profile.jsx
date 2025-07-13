2eca3cf6c0dc835ebe7b18932bbf6dd4c57caefd
Eman Radwan <emanalaa11499@gmail.com>
2025-07-13 09:32:56 +0300

    edit logout

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -140,6 +140,12 @@ const Profile = () => {
     }
   }, [profileData]);
 
+  const handleLogOut = async () => {
+    await logout();
+    navigate("/");
+    toast.success("Logged Out Successfully!");
+  };
+
   // Form handlers
   const personalDataForm = useFormik({
     enableReinitialize: true,
@@ -640,11 +646,7 @@ const Profile = () => {
                   </p>
 
                   <button
-                    onClick={async () => {
-                      await logout();
-                      navigate("/");
-                      toast.success("Logged Out Successfully!");
-                    }}
+                    onClick={handleLogOut}
                     className="border-2 border-red-700 text-red-700 px-4 py-2 rounded hover:bg-red-700 hover:text-white transition-colors w-4/5"
                   >
                     Sign Out
